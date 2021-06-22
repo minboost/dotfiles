a0850403fee6c3ac9ccdc070939a3c6bbbea119f
minboost <min@minboost.com>
2021-06-22 06:28:26 -0700

    Add zsh aliases

---
+++ oh-my-zsh/aliases.zsh
@@ -0,0 +1,45 @@
+# Frequently used paths
+alias pr="cd ~/Projects"
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias gd="cd ~/Google\ Drive"
+
+# Frequently used commands
+alias d="docker"
+alias dcomp="docker-compose"
+alias dmach="docker-machine"
+alias jup="jupyter notebook"
+alias k="kubectl"
+alias p="paver"
+
+# Dev Stuff
+alias hosts="code /private/etc/hosts"
+alias build="npm run build"
+alias dev="npm run dev"
+alias venv="pyenv virtualenv"
+alias py="pyenv activate"
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# URL-encode strings
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
+
+# Merge PDF files
+# Usage: `mergepdf -o output.pdf input{1,2,3}.pdf`
+alias mergepdf='/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py'
+
+# Disable Spotlight
+alias spotoff="sudo mdutil -a -i off"
+# Enable Spotlight
+alias spoton="sudo mdutil -a -i on"
